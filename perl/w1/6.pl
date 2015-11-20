31a97f5a482842cc146bed3cc63c710b3805018f
esviza <ignacio@esviza.com.ar>
2015-11-20 17:44:42 -0300

    optimizaciones ej6

--- perl/w1/6.pl
+++ perl/w1/6.pl
@@ -10,26 +10,6 @@ use warnings;
 
 use Render::FactoryOfCommand;
 
-# command figureType x1,y1 x2,y2 x3,y3
-if ( not exists $ARGV[0] ){
-    say STDERR "\nMissing command.\n";
-    exit 1;
-}
-if ( not $ARGV[0] =~ /(create)/ ){
-    say STDERR "\nUnknown command: $ARGV[0]\n";
-    exit 1;
-}
-if ( not exists $ARGV[1] ){
-    say STDERR "\nMissing type of figure.\n";
-    exit 1;
-}
-if ( not $ARGV[1] =~ /(rectangle|circle|square|triangle)/ ){
-    say STDERR "\nUnknown type: $ARGV[1]\n";
-    exit 1;
-}
-
-#-- get command with parameters
 my $command = Render::FactoryOfCommand->command( $ARGV[0] );
 
-$command->execute( {'figureType' => $ARGV[1],
-                    'userPoints' => [ $ARGV[2], $ARGV[3], $ARGV[4], $ARGV[5] ] } );
+$command->execute( @ARGV );
